87aa6d5c91f76722f6b3439d1cb2c72de60ae906
auxten <auxtenwpc@gmail.com>
2023-12-30 12:25:53 +0800

    Move update_libchdb.sh to lib

--- test/test.js
+++
@@ -1,8 +0,0 @@
-const { Execute } = require('..').chdb;
-
-describe("Execution", () => {
-	test('Execute returns correct version', () => {
-	  const version = Execute('SELECT 1', 'CSV');
-	  expect(version).toContain("1");
-	});
-});
